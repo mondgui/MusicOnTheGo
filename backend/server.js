f78f281261831627f29010322641288a5f8d46b4
Guimond Pierre Louis <mondgui@gmail.com>
2025-11-12 23:35:39 -0600

    Initialized backend (Express) and frontend (React Native)

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+// backend/server.js
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to MusicOnTheGo Backend API!");
+});
+
+const PORT = process.env.PORT || 5050;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
