9777b61cd487e16d3262d3cab3d959766ab457c3
Guimond Pierre Louis <mondgui@gmail.com>
2025-12-02 12:51:00 -0600

    Fix: convert rate field from string to number before API submission

--- MusicOnTheGo/frontend/app/profile-setup.tsx
+++ MusicOnTheGo/frontend/app/profile-setup.tsx
@@ -39,7 +39,7 @@ export default function ProfileSetup() {
           instruments,
           location,
           experience,
-          rate,
+          rate: rate ? Number(rate) : undefined,
           about,
         }),
       });
